cc2a1ba6cc269447adcb20e30a628d55a593956a
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-07-28 19:27:48 +0500

    search bar function and styling complete

--- src/domManager.js
+++ src/domManager.js
@@ -1,20 +1,39 @@
-class domManipulator {
+export default class domManipulator {
   #searchBar;
-  #regex = /^[a-zA-Z]+$/;
+  #options;
+  #regex = /^\p{L}+(?:[\s\-']\p{L}+)*$/u;
   constructor() {
     this.#searchBar = document.querySelector('#search');
+    this.#options = document.querySelector('#options');
+    let span = document.querySelector('#error');
+    let spanDiv = document.querySelector('.spanDiv');
+    let styleLine = document.querySelector('#line');
     this.#searchBar.addEventListener('keydown', (event) => {
       if (event.key === 'Enter') {
-        event.preventDefault;
-        const city = this.#searchBar.ariaValueMax.trim();
+        let city;
+        event.preventDefault();
+        if (this.#regex.test(this.#searchBar.value)) {
+          city = this.#searchBar.value.trim();
+        }
         if (city) {
           console.log(city);
         }
       }
     });
-    this.#searchBar.addEventListener('input', () => 
-        {
-            if(!this.#regex.test(this.#searchBar.value))
-        });
+    this.#searchBar.addEventListener('input', () => {
+      if (!this.#regex.test(this.#searchBar.value)) {
+        this.#searchBar.classList.add('displayOptionsInput');
+        this.#options.classList.add('displayOptionsButton');
+        spanDiv.classList.remove('hidden');
+        spanDiv.classList.add('error');
+        styleLine.classList.add('line');
+        span.textContent = 'Please type a valid City Name.';
+      } else {
+        spanDiv.classList.add('hidden');
+        span.textContent = '';
+        this.#searchBar.classList.remove('displayOptionsInput');
+        this.#options.classList.remove('displayOptionsButton');
+      }
+    });
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import './style.css';
 import { location } from './currentLocation.js';
 import apiProcessor from './apiHandler.js';
-const locationString = await location;
+import domModder from './domManager.js';
 try {
+  const locationString = await location;
   if (locationString) {
     const weatherApi = new apiProcessor(locationString);
-    weatherApi.getData();
+    // weatherApi.getData();
   } else {
     throw new Error('Unable to get location');
   }
 } catch (error) {
   console.log(`This is the error : ${error}`);
 }
+const dom = new domModder();
