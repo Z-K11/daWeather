1219ebd0e0fab7f19220865fefa21ed827bda508
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-07-28 12:34:54 +0500

    bug fix, handle error location acess denied previously the program would crash if the user denied access to location api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import { location } from './currentLocation.js';
 import apiProcessor from './apiHandler.js';
 const locationString = await location;
-const weatherApi = new apiProcessor(locationString);
-weatherApi.getData();
+try {
+  if (locationString) {
+    const weatherApi = new apiProcessor(locationString);
+    weatherApi.getData();
+  } else {
+    throw new Error('Unable to get location');
+  }
+} catch (error) {
+  console.log(error);
+}
